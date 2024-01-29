dfb6035f2c7c9efa2cbf1388fe2120d7e2398cbb
Marius Du Plooy <woestyn101@gmail.com>
2024-01-29 18:15:54 -0500

    first commit

---
+++ public/assets/js/notes.js
@@ -0,0 +1,159 @@
+
+// setting variabls from html id
+const divE = document.getElementById('list');
+var userTitle = document.getElementById("title");
+var userText = document.getElementById("text");
+var saveBtn = document.getElementById("postbutton");
+var clearBtn = document.getElementById("clearbutton");
+var newBtn = document.getElementById("newbutton");
+
+// setting buttons to hide
+saveBtn.style.display = "none";
+clearBtn.style.display = "none";
+newBtn.style.display = "none";
+
+// clear button click
+clearBtn.addEventListener("click", clearForm);
+
+// clear form fields values
+function clearForm(){
+    userTitle.value = "";
+    userText.value = "";
+    
+}
+
+// new button click function
+newBtn.addEventListener("click", newNote);
+
+// new button function to clear form fields and hide newBtn
+function newNote(){
+  userTitle.removeAttribute('readonly');
+  userText.removeAttribute('readonly');
+    userTitle.value = "";
+    userText.value = "";
+     newBtn.style.display = "none";
+}
+
+// adding eventlisteners to input fields
+   userTitle.addEventListener('keyup', show );
+  userText.addEventListener('keyup', show );
+
+  //hiding save and clear buttons
+  function show(){
+    
+    if (userTitle.value != ""){
+        saveBtn.style.display = "inline";
+        clearBtn.style.display = "inline";
+    }
+   
+  }
+
+// getting notes from db.json
+const getNotes = () =>
+  fetch('/notes/data', {
+    method: 'GET',
+  })
+    .then((res) => res.json())
+    .then((data) => {
+       return data;
+      });
+      
+      // setting empty arrar for db.json data
+      var textobject = [];
+     
+      // outputting db.json data to html
+    function renderNotes(note){
+        var divTitle = document.createElement("div");
+        var divText = document.createElement("div");
+            
+       
+        divTitle.classList.add('title-list');
+        divTitle.innerHTML = note.title;
+        divText.innerHTML = note.text;
+        textobject.push(note.title);
+        textobject.push(note.text);
+        textobject.push(note.id);
+        divE.appendChild(divTitle);
+               
+     
+        
+        
+        // outputting note text to form field
+        divE.addEventListener('click', viewNote);
+       
+            }
+   
+            // function to view title and text  in form fields
+     function viewNote(event){
+        
+       userTitle.value = event.target.textContent;
+      
+
+        //finding index of title in array
+        let titleIndex = textobject.findIndex(x => x == event.target.textContent);
+       // getting text value from array
+        userText.value = textobject[titleIndex + 1];
+
+        //displaying new btn
+        newBtn.style.display = "inline";
+
+        //hiding new btn
+        saveBtn.style.display = "none";
+        clearBtn.style.display = "none";
+
+        userTitle.setAttribute('readonly', true);
+        userText.setAttribute('readonly', true);
+        
+     }       
+
+      // getting notes from db.json and outputting to html
+    getNotes().then((response) => response.forEach((item) => renderNotes(item)));
+
+    
+   // savebtn click 
+    saveBtn.addEventListener("click", postData);
+    
+    // fuction to post new data to db.json
+      function postData() {
+
+        // getting data from input fields
+        var data = {
+          title: userTitle.value,
+          text: userText.value,
+        
+        };
+
+        // using fetch to post new data
+        fetch("/notes", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+           
+            return response.text();
+          })
+          .then((data) => {
+            console.log(data);
+            setTimeout(function(){
+              window.location.reload();
+           }, 500);
+                               
+          })
+          .catch((error) => {
+            console.error(
+              "There was a problem with the fetch operation:",
+              error
+            );
+          });
+
+         
+       
+       
+      }
+
---
+++ server.js
@@ -0,0 +1,98 @@
+// Import Express.js
+const express = require('express');
+
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const path = require('path');
+
+// import bodyparser to read and output json format
+const bodyParser = require('body-parser');
+
+//import fs to read and write files
+const fs = require('fs');
+
+//importing db.json file
+const noteData = require('./public/assets/db/db.json');
+
+//importing uuid to create unique id's
+const uuid = require('uuid');
+const { type } = require('os');
+
+// initializing unique id variable
+var uniqueID = uuid.v4();
+
+
+// Initialize an instance of Express.js
+const app = express();
+// Specify on which port the Express.js server will run
+const PORT = 8080;
+
+// Static middleware pointing to the public folder
+app.use(express.static('public'));
+
+// middle ware to read json files
+app.use(bodyParser.json());
+
+// setting route for index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+// setting route for notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+//setting route for reading data from json file
+app.get('/notes/data', (req, res) => {
+    res.json(noteData);
+});
+
+
+    // Define your route to handle POST requests
+app.post('/notes', (req, res) => {
+    // Assuming the request body contains JSON data
+    const newData = req.body;
+
+    // Read existing data from the file
+    fs.readFile('./public/assets/db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading file');
+            return;
+        }
+       
+       // getting new id for new data
+        newData.id = uniqueID;
+       
+        // parson new data into json
+        let jsonData = JSON.parse(data);
+        
+        
+        // Add the new data to the existing data
+        jsonData.push(newData);
+
+        // Write the updated data back to the file
+        fs.writeFile('./public/assets/db/db.json', JSON.stringify(jsonData), (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error writing file');
+                return;
+            }
+           
+            res.send('Data saved successfully');            
+                  
+        
+           
+        });
+       
+       
+    });
+
+  
+
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
